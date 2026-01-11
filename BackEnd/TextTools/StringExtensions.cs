90d35a6afea835696b25ae74597fe6fe0021a852
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-01-11 14:13:07 +0300

    Add strings case-independent comparing

---
+++ BackEnd/TextTools/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace PomoSyncAPI.BackEnd.TextTools;
+
+public static class StringExtensions
+{
+    public static bool IsSame(this string? a, string? b)
+    {
+        return a?.ToLower() == b?.ToLower();
+    }
+}
