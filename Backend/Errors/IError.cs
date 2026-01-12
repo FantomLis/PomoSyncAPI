1d6012dd5c5e83b5acc14840f39ffd1fd077e3fb
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-01-12 10:58:25 +0300

    Create IError for API errors

---
+++ Backend/Errors/IError.cs
@@ -0,0 +1,8 @@
+﻿namespace PomoSyncAPI.Backend.Errors;
+
+public interface IError
+{
+    public string Code { get; }
+    public string Message(params dynamic?[] args);
+    public string Message(string msg) => msg;
+}
