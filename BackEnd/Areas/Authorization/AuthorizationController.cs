cb8dee59d2b5b7b0cb53f94475673f2eec784442
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-01-12 10:50:15 +0300

    Create Authorization region

---
+++ BackEnd/Areas/Authorization/AuthorizationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PomoSyncAPI.Backend.Areas.Authorization;
+
+[Area("Authorization")]
+[ApiController]
+[Route("auth")]
+public class AuthorizationController : Controller
+{
+    [HttpGet("token")]
+    public IActionResult GenerateToken(string password)
+    {
+        return Ok();
+    }
+}
