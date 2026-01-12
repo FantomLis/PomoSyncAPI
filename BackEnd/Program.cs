ff45d19f7146e1e14b3fc2a0c0dd5ecf851bf63c
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-01-12 09:58:23 +0300

    Clean-up swagger services

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -28,8 +28,7 @@ public static class Executable
         // Add services to the container.
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
+        
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
