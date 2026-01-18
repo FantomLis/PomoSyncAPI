ba75e8b8e268d30a9e554f6a01e7816474a85569
FantomLis <71078845+FantomLis@users.noreply.github.com>
2026-01-18 13:02:08 +0300

    Create User model with auth vars

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -1,6 +1,15 @@
-﻿namespace PomoSyncAPI.Backend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
+namespace PomoSyncAPI.Backend.Models;
+
+[Table("Users")]
 public class User
 {
-    
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Token;
+    [Required]
+    public string PasswordHash;
 }
